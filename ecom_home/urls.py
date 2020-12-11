4beb850ee2c4ade450487f65ae771b86b772608a
kamaldalakoti <64273320+kamaldalakoti@users.noreply.github.com>
2020-12-11 16:08:47 +0530

    Update urls.py

--- ecom_home/urls.py
+++ ecom_home/urls.py
@@ -24,16 +24,16 @@ urlpatterns = [
     path('seller_account/',views.be_seller,name='Seller_account'),
     path('review_seller_product/',views.review_seller_product,name='review_seller_product'),
     path('seller_product_post',views.seller_product_post,name='seller_product_post'),
-    path('', indexView.as_view(), name='home'),
-    path('Home', indexView.as_view(), name='home'),
+#     path('', indexView.as_view(), name='home'),
+#     path('Home', indexView.as_view(), name='home'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
+#     path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
         name='remove-single-item-from-cart'),
-    path('payment/', PaymentView.as_view(), name='payment'),
-    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
+#     path('payment/', PaymentView.as_view(), name='payment'),
+#     path('request-refund/', RequestRefundView.as_view(), name='request-refund')
 ]
